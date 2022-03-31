37dcc67d2532ac845a7ecf22aafe9f8a3f37f83e
bdrachovska <44036264+bdrachovska@users.noreply.github.com>
2022-03-31 22:44:03 +0200

    ukol_filmy_4

--- src/components/Movielist/index.jsx
+++ src/components/Movielist/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import movies from '../../movies'
 import Movie from '../Movie';
 
 
@@ -10,7 +11,7 @@ const Movielist = ({movies}) => {
 <div className='movielist'>
  <ul>
  {movies.map(movie =>
-    <Movie title={movie.title} year={movie.year} rating={movie.rating} director={movie.director} genre={movie.genre} cast={movie.cast}/>
+    <Movie key={movie.id} title={movie.title} year={movie.year} rating={movie.rating} director={movie.director} genre={movie.genre} cast={movie.cast}/>
       )}
 </ul>
 </div>
