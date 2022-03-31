863d151b69581c344c647eeb550bac8c1aa4fc82
bdrachovska <44036264+bdrachovska@users.noreply.github.com>
2022-03-31 23:26:36 +0200

    ukol_filmy_6

--- src/components/Movie/index.jsx
+++ src/components/Movie/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './style.css';
 const Movie = ({title, poster, year, rating, director, genre}) => {
 
 	return (
 <>
 <div className='movie'>
     <h2>{title}</h2>
-					<img src= {`/assets/${poster}.jpg`} alt={title}/>
+					<img src={`/assets/${poster}`} alt={title}/>
 						<p className='movie_text'>{year}</p>
 						<p className='movie_rating'>{rating}</p>
 						<p className='movie_text'>{director}</p>
