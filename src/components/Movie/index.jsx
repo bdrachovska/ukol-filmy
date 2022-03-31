fe760cb48c2729f6a9d5e03d5b37ba54dbbed5f8
bdrachovska <44036264+bdrachovska@users.noreply.github.com>
2022-03-31 23:51:40 +0200

    ukol_filmy_CSS

--- src/components/Movie/index.jsx
+++ src/components/Movie/index.jsx
@@ -5,8 +5,8 @@ const Movie = ({title, poster, year, rating, director, genre}) => {
 	return (
 <>
 <div className='movie'>
-    <h2>{title}</h2>
-					<img src={`/assets/${poster}`} alt={title}/>
+    <h2 className="movie_title">{title}</h2>
+					<img src={`/assets/${poster}`} alt={title} className='movie_img'/>
 						<p className='movie_text'>{year}</p>
 						<p className='movie_rating'>{rating}</p>
 						<p className='movie_text'>{director}</p>
